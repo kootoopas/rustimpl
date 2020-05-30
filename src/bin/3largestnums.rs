94ad09e3f09c7d7743165a538601a5f6c44ff7a6
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2020-05-30 04:22:39 +0300

    Add 3largestnums.rs

--- src/bin/3largestnums.rs
+++ src/bin/3largestnums.rs
@@ -1,30 +1,30 @@
-pub fn find_3_largest_nums(vec: &Vec<i32>) -> Result<(i32, i32, i32), String> {
+pub fn find_3_largest_nums(vec: Vec<i32>) -> Result<(i32, i32, i32), String> {
     
     if vec.len() < 3 {
-        return Err("Vec should have 3 or more elements".to_string())
+        return Err("input vec should have 3 or more elements".to_string())
     }
 
-    let mut a: Option<&i32> = None;
-    let mut b: Option<&i32> = None;
-    let mut c: Option<&i32> = None;
+    let mut a: i32 = std::i32::MIN;
+    let mut b: i32 = std::i32::MIN;
+    let mut c: i32 = std::i32::MIN;
 
-    for n in vec.iter() {
-        if n > a || a == None {
+    for n in vec {
+        if n > a {
             let t = a;
             let t_ = b;
             a = n;
             b = t;
             c = t_;
-        } else if n > b || b == None {
+        } else if n > b {
             let t = b;
             b = n;
             c = t;
-        } else if n > c || c == None {
-            c = n
+        } else if n > c {
+            c = n;
         }
     }
 
-    Ok((*a, *b, *c))
+    Ok((a, b, c))
 }
 
 #[cfg(test)]
@@ -33,12 +33,23 @@ mod tests {
 
     #[test]
     fn returns_sorted_3_largest_nums() {
-        let mut i = vec![8, 5, 2, 9, 5, 6, 3];
-        let r = find_3_largest_nums(i.as_mut());
+        let i = vec![8, 5, 2, 9, 5, 6, 3];
+        let r = find_3_largest_nums(i);
         assert!(r.is_ok());
         match r {
             Ok(largest_triple) => assert_eq!(largest_triple, (9, 8, 6)),
             Err(_) => {}
+        }
+    }
+    
+    #[test]
+    fn errs_when_input_vec_len_is_less_than_desired_output() {
+        let i = vec![2, 1];
+        let r = find_3_largest_nums(i);
+        assert!(r.is_err());
+        match r {
+            Err(msg) => assert!(msg.len() > 0),
+            Ok(_) => {}
         }
     }
 }
